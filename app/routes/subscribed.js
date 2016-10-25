98e459d0fa9b793b3d86479848d334dad2551833
Tolu Oyeniyi <tjoye20@gmail.com>
2016-10-25 11:19:02 -0500

    Finish adding components.

--- app/routes/subscribed.js
+++ app/routes/subscribed.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('customer')
-      .then(customers => customers.filterBy('isSubscribed', false));
+      .then(customers => customers.filterBy('isSubscribed', true));
   }
 });
